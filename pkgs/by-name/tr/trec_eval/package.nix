c5bee14aa080d02f6dbc88ed33d4b6c4d9773ad7
LodWKobku <lodwkobku@gmail.com>
2026-06-05 13:52:46 +0200

    trec_eval: slop

--- pkgs/by-name/tr/trec_eval/package.nix
+++ pkgs/by-name/tr/trec_eval/package.nix
@@ -12,11 +12,24 @@ stdenv.mkDerivation (finalAttrs: {
     repo = "trec_eval";
     tag = "v${finalAttrs.version}";
     hash = "sha256-vAeI2uIrYTKTlFTAv4z5A391AUPaSTGJccLVcIGutiU=";
-    fetchSubmodules = true;
   };
 
+  buildPhase = ''
+    cat > version.h <<EOF
+#define VERSIONID "${finalAttrs.version}"
+EOF
+    make CFLAGS='-g -I. -Wall -std=gnu89 -Wno-incompatible-pointer-types -include version.h'
+  '';
+
+  installPhase = ''
+    mkdir -p "$out/bin"
+    cp trec_eval "$out/bin/"
+  '';
+
+  doCheck = false;
+
   meta = {
-    description = "Evaluation software used in the Text Retrieval Conference ";
+    description = "Evaluation software used in the Text Retrieval Conference";
     homepage = "https://github.com/usnistgov/trec_eval/";
     changelog = "https://github.com/usnistgov/trec_eval/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.nistSoftware;
